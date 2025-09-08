2af6aa13b893cedd72f573316635d3646abcdcf7
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-09-08 16:19:17 +0200

    Implemented findAllTennisMatches()

--- com.giulia.giamberini.tennis/src/main/java/com/giulia/giamberini/tennis/controller/TennisMatchController.java
+++ com.giulia.giamberini.tennis/src/main/java/com/giulia/giamberini/tennis/controller/TennisMatchController.java
@@ -1,10 +1,20 @@
 package com.giulia.giamberini.tennis.controller;
 
+import com.giulia.giamberini.tennis.repository.TennisMatchRepository;
+import com.giulia.giamberini.tennis.view.TennisManagementView;
+
 public class TennisMatchController {
 
+	private TennisMatchRepository repo;
+	private TennisManagementView view;
+
+	public TennisMatchController(TennisMatchRepository repo, TennisManagementView view) {
+		this.repo = repo;
+		this.view = view;
+	}
+
 	public void findAllTennisMatches() {
-		// TODO Auto-generated method stub
-		
+		view.showAllTennisMatches(repo.findAll());
 	}
 
 }
