0b56720d04886f14ab5c11936f2073ecad2f7970
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-09-08 15:39:52 +0200

    Implemented findAllTennisPlayers

--- com.giulia.giamberini.tennis/src/test/java/com/giulia/giamberini/tennis/controller/TennisPlayerController.java
+++ com.giulia.giamberini.tennis/src/test/java/com/giulia/giamberini/tennis/controller/TennisPlayerController.java
@@ -1,10 +1,20 @@
 package com.giulia.giamberini.tennis.controller;
 
+import com.giulia.giamberini.tennis.repository.TennisPlayerRepository;
+import com.giulia.giamberini.tennis.view.TennisManagementView;
+
 public class TennisPlayerController {
 
+	private TennisPlayerRepository repo;
+	private TennisManagementView view;
+
+	public TennisPlayerController(TennisPlayerRepository repo, TennisManagementView view) {
+		this.repo = repo;
+		this.view = view;
+	}
+
 	public void findAllTennisPlayers() {
-		// TODO Auto-generated method stub
-		
+		view.showAllTennisPlayers(repo.findAll());
 	}
 
 }
