e05fa11445df4887bd43d0e52289cb98bbcf4d2e
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-09-09 19:19:20 +0200

    Added failing test for showErrorTennisMatchAlreadyExist()

--- com.giulia.giamberini.tennis/src/test/java/com/giulia/giamberini/tennis/view/swing/TennisManagementViewSwingTest.java
+++ com.giulia.giamberini.tennis/src/test/java/com/giulia/giamberini/tennis/view/swing/TennisManagementViewSwingTest.java
@@ -374,4 +374,15 @@ public class TennisManagementViewSwingTest extends AssertJSwingJUnitTestCase {
 		GuiActionRunner.execute(() -> view.showAllTennisMatches(Arrays.asList(match1, match2)));
 		assertThat(window.list("matchesList").contents()).containsExactly(match1.toString(), match2.toString());
 	}
+	
+	@Test
+	@GUITest
+	public void testShowErrorMatchAlreadyPresentShouldShowTheErrorMessageInTheErrorMatchLabel() {
+		TennisPlayer player1 = new TennisPlayer("1", "test name1", "test surname1");
+		TennisPlayer player2 = new TennisPlayer("2", "test name2", "test surname2");
+		LocalDate date = LocalDate.of(2025, 10, 25);
+		TennisMatch match = new TennisMatch(player1, player2, date);
+		GuiActionRunner.execute(() -> view.showErrorTennisMatchAlreadyExist("Error message", match));
+		window.label("errorMatchLbl").requireText("Error message: (1,test name1,test surname1) - (2,test name2,test surname2) - 2025-10-25");
+	}
 }
