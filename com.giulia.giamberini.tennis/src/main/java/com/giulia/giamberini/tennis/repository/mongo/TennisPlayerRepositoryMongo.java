fd6ecfa4256f0b6a34f89def7af2be38aa5308d3
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-09-09 11:40:27 +0200

    Implemented findAll()

--- com.giulia.giamberini.tennis/src/main/java/com/giulia/giamberini/tennis/repository/mongo/TennisPlayerRepositoryMongo.java
+++ com.giulia.giamberini.tennis/src/main/java/com/giulia/giamberini/tennis/repository/mongo/TennisPlayerRepositoryMongo.java
@@ -1,21 +1,34 @@
 package com.giulia.giamberini.tennis.repository.mongo;
 
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
 import com.giulia.giamberini.tennis.model.TennisPlayer;
 import com.giulia.giamberini.tennis.repository.TennisPlayerRepository;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoCollection;
 
 public class TennisPlayerRepositoryMongo implements TennisPlayerRepository {
 
+	private MongoCollection<TennisPlayer> collection;
+
 	public TennisPlayerRepositoryMongo(MongoClient client, String databaseName, String collectionName) {
-		// TODO Auto-generated method stub
+		CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+				fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+		collection = client.getDatabase(databaseName).withCodecRegistry(pojoCodecRegistry).getCollection(collectionName,
+				TennisPlayer.class);
 	}
 
 	@Override
 	public List<TennisPlayer> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return collection.find().into(new ArrayList<>());
 	}
 
 	@Override
