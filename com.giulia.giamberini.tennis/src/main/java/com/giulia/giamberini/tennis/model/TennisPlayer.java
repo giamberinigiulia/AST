73372953c05099572fcc04a208fd4f7e73ff7441
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-09-09 11:57:50 +0200

    Added public setters to re-construct object using POJO Codec

--- com.giulia.giamberini.tennis/src/main/java/com/giulia/giamberini/tennis/model/TennisPlayer.java
+++ com.giulia.giamberini.tennis/src/main/java/com/giulia/giamberini/tennis/model/TennisPlayer.java
@@ -21,12 +21,24 @@ public class TennisPlayer {
 		return id;
 	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getSurname() {
 		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
 	}
 
 	@Override
