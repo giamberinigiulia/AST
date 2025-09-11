c30697f78e9e89f8ac4f3769d5993d99db909afa
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-09-11 10:13:12 +0200

    Verify delete tennis player cascades to associated matches

--- com.giulia.giamberini.tennis/src/it/java/com/giulia/giamberini/tennis/controller/TennisPlayerControllerIT.java
+++ com.giulia.giamberini.tennis/src/it/java/com/giulia/giamberini/tennis/controller/TennisPlayerControllerIT.java
@@ -1,17 +1,21 @@
 package com.giulia.giamberini.tennis.controller;
 
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testcontainers.containers.MongoDBContainer;
 
+import com.giulia.giamberini.tennis.model.TennisMatch;
 import com.giulia.giamberini.tennis.model.TennisPlayer;
 import com.giulia.giamberini.tennis.repository.TennisMatchRepository;
 import com.giulia.giamberini.tennis.repository.TennisPlayerRepository;
@@ -59,12 +63,32 @@ public class TennisPlayerControllerIT {
 		controller.findAllTennisPlayers();
 		verify(view).showAllTennisPlayers(Arrays.asList(player1));
 	}
-	
+
 	@Test
 	public void testAddNewTennisPlayer() {
 		TennisPlayer player1 = new TennisPlayer("1", "test name", "test surname");
 		controller.addNewTennisPlayer(player1);
 		verify(view).newTennisPlayerAdded(player1);
+	}
+
+	@Test
+	public void testRemoveTennisPlayerAlsoRemovesAssociatedMatches() {
+		TennisPlayer player1 = new TennisPlayer("1", "test name1", "test surname1");
+		TennisPlayer player2 = new TennisPlayer("2", "test name2", "test surname2");
+		TennisPlayer player3 = new TennisPlayer("3", "test name3", "test surname3");
+		playerRepo.save(player1);
+		playerRepo.save(player2);
+		playerRepo.save(player3);
+		LocalDate today = LocalDate.of(2025, 10, 10);
+		TennisMatch match1 = new TennisMatch(player1, player2, today);
+		TennisMatch match2 = new TennisMatch(player3, player1, today);
+		matchRepo.save(match1);
+		matchRepo.save(match2);
+		controller.deleteTennisPlayer(player1);
+		InOrder order = inOrder(view);
+		order.verify(view).tennisMatchRemoved(match1);
+		order.verify(view).tennisMatchRemoved(match2);
+		order.verify(view).tennisPlayerRemoved(player1);
 	}
 
 }
