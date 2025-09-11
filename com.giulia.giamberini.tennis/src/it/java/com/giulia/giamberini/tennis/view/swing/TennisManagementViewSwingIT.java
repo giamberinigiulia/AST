007dc055cf3a9d0e1e903f27dd3822ba8ec9af9a
Giulia Giamberini <giuliagiamberini@gmail.com>
2025-09-11 12:11:48 +0200

    Add IT for showing error when deleting match that doesn't exist

--- com.giulia.giamberini.tennis/src/it/java/com/giulia/giamberini/tennis/view/swing/TennisManagementViewSwingIT.java
+++ com.giulia.giamberini.tennis/src/it/java/com/giulia/giamberini/tennis/view/swing/TennisManagementViewSwingIT.java
@@ -193,4 +193,18 @@ public class TennisManagementViewSwingIT extends AssertJSwingJUnitTestCase {
 		assertThat(window.list("matchesList").contents()).isEmpty();
 	}
 
+	@Test
+	@GUITest
+	public void testDeleteTennisMatchWithError() {
+		TennisPlayer player1 = new TennisPlayer("1", "test name1", "test surname1");
+		TennisPlayer player2 = new TennisPlayer("2", "test name2", "test surname2");
+		LocalDate date = LocalDate.of(2025, 10, 10);
+		TennisMatch match = new TennisMatch(player1, player2, date);
+		GuiActionRunner.execute(() -> view.newTennisMatchAdded(match));
+		window.list("matchesList").selectItem(0);
+		window.button(JButtonMatcher.withText("Delete match")).click();
+		window.label("errorMatchLbl").requireText(
+				"The selected match doesn't exist: (1,test name1,test surname1) - (2,test name2,test surname2) - 2025-10-10");
+	}
+
 }
